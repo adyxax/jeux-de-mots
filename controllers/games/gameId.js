cffa0474dd892c13c3b0deee6d2e14e4637874b7
Julien Dessaux <julien.dessaux@adyxax.org>
2023-02-11 21:46:06 +0100

    Improved games controller tests

--- controllers/games/gameId.js
+++ controllers/games/gameId.js
@@ -9,16 +9,19 @@ function makePageData(user, game) {
 }
 
 export function gameId_get(req, res) {
-	const game = JSON.parse(getGame(req.params.gameId).data);
-	if (game) {
-		if (game.player1.id === req.session.user.id) {
-			game.letters = game.player1.letters;
-			delete game.player2.letters;
-		} else {
-			game.letters = game.player2.letters;
-			delete game.player1.letters;
+	const gameData = getGame(req.params.gameId);
+	if (gameData) {
+		const game = JSON.parse(gameData.data);
+		if (game) {
+			if (game.player1.id === req.session.user.id) {
+				game.letters = game.player1.letters;
+				delete game.player2.letters;
+			} else {
+				game.letters = game.player2.letters;
+				delete game.player1.letters;
+			}
+			return res.render('game', makePageData(req.session.user, game));
 		}
-		return res.render('game', makePageData(req.session.user, game));
-	}
+	} // we got null or undefined from the getGame database request
 	return res.redirect('/games');
 }
--- tests/games.spec.js
+++ tests/games.spec.js
@@ -6,7 +6,8 @@ import app from '../main.js';
 describe.concurrent('Games handlers tests', function() {
 	describe.concurrent('When not logged in', function() {
 		test('GET /games', async function() { await supertest(app).get('/games').expect('Content-Type', /text\/plain/).expect(302, /Redirecting to \/login$/); });
-		test('GET /games/1', async function() { await supertest(app).get('/games').expect('Content-Type', /text\/plain/).expect(302, /Redirecting to \/login$/); });
+		test('GET /games/1', async function() { await supertest(app).get('/games/1').expect('Content-Type', /text\/plain/).expect(302, /Redirecting to \/login$/); });
+		test('GET /games/2', async function() { await supertest(app).get('/games/2').expect('Content-Type', /text\/plain/).expect(302, /Redirecting to \/login$/); });
 	});
 	describe.concurrent('With valid credentials', function() {
 		beforeEach(async function(ctx) {
@@ -19,5 +20,6 @@ describe.concurrent('Games handlers tests', function() {
 		});
 		test('GET /games', async function(ctx) { await supertest(app).get('/games').set('Cookie', ctx.cookie).expect('Content-Type', /text\/html/).expect(200, /<td><a href="\/games\/1">Alice vs Bob<\/a><\/td>/); });
 		test('GET /games/1', async function(ctx) { await supertest(app).get('/games/1').set('Cookie', ctx.cookie).expect('Content-Type', /text\/html/).expect(200, /<h2>Alice vs Bob<\/h2>/); });
+		test('GET /games/2', async function(ctx) { await supertest(app).get('/games/2').set('Cookie', ctx.cookie).expect('Content-Type', /text\/plain/).expect(302, /Redirecting to \/games/); });
 	});
 });
